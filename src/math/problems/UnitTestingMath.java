94ace42a6fa07b616157727907d7f62e985dc358
shagria kabir <shagria.kabir@gmail.com>
2018-12-23 18:58:48 -0500

    math.problems: UnitTestingMath

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,8 +1,17 @@
 package math.problems;
 
+import org.testng.Assert;
+
 public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
+
+        try {//unit testing
+            Assert.assertEquals(Factorial.fact(5), 120);
+            System.out.println("Unit testing passed");
+        }catch (AssertionError ar){
+            System.out.println("Unit testing failed");
+        }
 
     }
 }
