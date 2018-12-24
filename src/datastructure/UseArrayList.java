a6bb2227382c2afa2b19b3329ed84b80f35ed32a
shagria kabir <shagria.kabir@gmail.com>
2018-12-23 19:18:16 -0500

    datastructure: UseArrayList

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.ArrayList;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,8 +11,19 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		ArrayList<String> alist = new ArrayList<String>();
+		alist.add("Steve");
+		alist.add("Tim");
+		alist.add("Lucy");
+		alist.add("Pat");
+		alist.add("Angela");
+		alist.add("Tom");
 
+
+		System.out.println(alist);
+		alist.add(3, "Steve");
+		System.out.println(alist);
 	}
-
 }
+
+
--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -1,7 +1,6 @@
 package math.problems;
 
 import databases.ConnectToSqlDB;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +14,6 @@ public class LowestNumber {
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
 		//find lowest number from the array
-
-		int Array[] = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
-		int smallest = Integer.MAX_VALUE;
-
-		for (int i = 0; i < array.length; i++) {
-			if (smallest > array[i]) {
-				smallest = array[i];
-			}
-		}
-		System.out.println("Smallest number in array is : " + smallest);
-
 
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		List<String> lowestValue = new ArrayList<String>();
@@ -41,6 +29,5 @@ public class LowestNumber {
 			System.out.println(st);
 		}
 	}
-
 
 }
