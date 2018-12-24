f02711c34488795a4e51def43d2e657ee62dda7a
shagria kabir <shagria.kabir@gmail.com>
2018-12-24 16:46:25 -0500

    string.problems: Permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -7,10 +7,26 @@ public class Permutation {
 
     public static void main(String[] args) {
 
+
         /*
          * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
          * Write Java program to compute all Permutation of a String
          *
          */
+        listPermutations("", "dad");
+    }
+
+    public static void listPermutations(String pre, String word) {
+        int length = word.length();
+        if (length == 0) {
+            System.out.println(pre);
+        } else {
+            for (int i = 0; i < length; i++) {
+                listPermutations(pre + word.charAt(i), word.substring(0, i) + word.substring(i + 1, length));
+
+            }
+        }
+
+
     }
 }
