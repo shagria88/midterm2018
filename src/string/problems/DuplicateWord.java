198947416f8912b160d7c040810a7976a8d56632
shagria kabir <shagria.kabir@gmail.com>
2018-12-24 15:05:31 -0500

    string.problems: DuplicateWord

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,9 +1,13 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class DuplicateWord {
+
 
     public static void main(String[] args) {
         /*
@@ -12,7 +16,26 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-
+        System.out.println(DuplicateWords(st));
     }
 
+
+    public static String DuplicateWords(String str) {
+        String resultHolder = "";
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        String[] arr = str.split (" ");
+
+        for (String a : arr) {
+            if (map.get(a) != null) {
+                map.put(a, map.get(a) + 1);
+            } else {
+                map.put(a, 1);
+            }
+        }
+
+        for (Map.Entry entry : map.entrySet()) {
+            resultHolder += entry.getKey() + ": " + entry.getValue() + ", ";
+        }
+        return resultHolder;
+    }
 }
