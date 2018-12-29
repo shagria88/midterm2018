3e786c8a77f31a829e204e68be29277cbc4ad163
shagria kabir <shagria.kabir@gmail.com>
2018-12-29 00:22:21 -0500

    design

--- src/design/FortuneEmployee.java
+++ src/design/FortuneEmployee.java
@@ -4,8 +4,8 @@ public class FortuneEmployee {
 
 	/**
 	 * FortuneEmployee class has a main methods where you will be able to create Object from
-	 * EmployeeInfo class to use fields and attributes.Demonstrate as many methods as possible 
-	 * to use with proper business work flow.Think as a Software Architect, Product Designer and 
+	 * EmployeeInfo class to use fields and attributes.Demonstrate as many methods as possible
+	 * to use with proper business work flow.Think as a Software Architect, Product Designer and
 	 * as a Software Developer.(employee.info.system) package is given as an outline,you need to elaborate
 	 * more to design an application that will meet for fortune 500 Employee Information
 	 * Services.
@@ -14,7 +14,30 @@ public class FortuneEmployee {
 	 *
 	 **/
 	public static void main(String[] args) {
-		
+
+		EmployeeInfo e1 = new EmployeeInfo("name", 101);
+		e1.getDept("Dev");
+		e1.farewell(01/01/2018);
+		e1.setSalary(50000.00);
+		e1.setEmployeeId(101);
+		e1.setGender('F');
+		e1.setName("Smith");
+		e1.performance("high");
+
+		EmployeeInfo e2 = new EmployeeInfo("Monir", 101, "QA", 30000.00, 'M');
+		e2.farewell();
+		e2.annualSalary();
+		e2.employeeName();
+		e2.assignDepartment();
+		e2.calculateSalary();
+		e2.getGender();
+
+
+		EmployeeInfo.calculateEmployeePension();
+		EmployeeInfo.calculateEmployeeBonus(10, 50000.00);
+		EmployeeInfo.setCompanyName("PNT");
+
+		System.out.println(Months.valueOf("January"));
 
 
 	}
